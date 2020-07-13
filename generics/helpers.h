b70eeff15d17d850920c3df16b50b8ffa43b9729
bastisawesome <bastisawesomeltd@gmail.com>
2020-07-12 22:02:12 -0400

    Implement binary debug output

--- generics/helpers.h
+++ generics/helpers.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <bitset>
 
 #include "types.h"
 
@@ -18,6 +19,16 @@ std::string printHex(uint16_t num, uint16_t width=2) {
 
 std::string printHex(pair num, uint16_t width=2) {
     return printHex(num.w, width);
+}
+
+std::string printBin(uint8_t num) {
+    std::stringstream out;
+    std::bitset<8> bit(num);
+
+    out << std::setfill('0') << std::right;
+    out << "0b" << bit;
+
+    return out.str();
 }
 
 #endif // HELPERS_H
